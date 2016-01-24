1d3de76b1b93b4189a13975216cac9655ea89284
Laura Pigott <pigottlaura@gmail.com>
2016-01-24 17:04:11 +0000

    Creating Objects and Properties
    Creating objects using the {} method, as well as using the new Object() constructor method. Demonstrating how object properties can be created after the object has been created.

--- firstapp.js
+++ firstapp.js
@@ -65,7 +65,12 @@ console.log("\n------ VARIALBE SHADOWING ------");
 // when we refer to z, we are referring to the local variable created within that code block,
 // and can no longer reference the global variable z which was declared previously. As with
 // the global variable example above, once we return to the main code, we can now reference
-// the original z variable again.
+// the original z variable again. Variable shadowing is something that should be avoided,
+// as it can return unexpected results, particuarly when you inadvertantly do it i.e. creating
+// the local variable of z without realising you already had a global variable of z. The rule
+// of thumb should be that if it needs to be global make it global, otherwise you should keep
+// it local. This should help to avoid unnecessarily declaring variables repeatedly throughout
+// your code, as you will always know where they will be needed.
 var z = 6;
 function assignNewValue(newVal)
 {
@@ -106,3 +111,46 @@ function printMoreVariables()
     console.log("a = " + a + "; b = " + b + "; c = " + c);
 }
 printMoreVariables();
+
+console.log("\n------ CREATING OBJECTS ------");
+var personA = {
+    name: "Laura",
+    age: 28,
+    favSongs: ["Shine Your Light", "Someday We\'ll Know", "Some Nights"],
+    addNewFavSong: function(song) {
+        this.favSongs.push(song);
+    },
+    showFavSongs: function(){
+        for(song in this.favSongs)
+        {
+            console.log(this.favSongs[song]);
+        }
+    }
+}
+personA.addNewFavSong("Hold the World");
+personA.showFavSongs();
+
+console.log("\n------ CREATING OBJECT PROPERTIES AFTER THE OBJECT ------");
+// If the interpreter meets a reference to a property of an object that it has not
+// seen before i.e. personB.growOlder, then it assumes it is the declaration of a
+// new property and so no error will occur. This can be handy for creating additional
+// properites on the fly, but should be something to watch out for as it can result in
+// bugs in your code, without you even realising i.e. if I had a property declared within
+// the object called growOld, and later referred to it as growOlder, then I have now got two
+// properties in the object (growOld and growOlder). JavaScript will not throw and error
+// for this, as it just assumes the new property was intentional, but it may cause me issues
+// later as I could be unaware that I had inadvertantly created a new property in my object.
+// Once an object has been created, I can continue to add new properties later on, as shown below.
+var personB = new Object();
+personB.name = "David";
+personB.age = 25;
+personB.showAge = function()
+{
+    console.log(this.name + " is " + this.age + " years old.");
+}
+personB.growOlder = function(){
+    this.age++;
+    console.log(this.name + " is now " + this.age + " years old.");
+}
+personB.showAge();
+personB.growOlder();
